2000b5f83a35c2f6f17308a2583e5e433b8b6dd6
steveleelx <steveleelx@gmail.com>
2015-11-28 20:40:04 -0500

    Game Changed!

---
+++ src/Shot.java
@@ -0,0 +1,23 @@
+/**
+ * Created by SteveLeeLX on 11/28/15.
+ */
+import javax.swing.ImageIcon;
+
+
+public class Shot extends Sprite {
+
+    private String shot = "spacepix/shot.png";
+    private final int H_SPACE = 6;
+    private final int V_SPACE = 1;
+
+    public Shot() {
+    }
+
+    public Shot(int x, int y) {
+
+        ImageIcon ii = new ImageIcon(this.getClass().getResource(shot));
+        setImage(ii.getImage());
+        setX(x + H_SPACE);
+        setY(y - V_SPACE);
+    }
+}
---
+++ src/SpaceInvaders.java
@@ -0,0 +1,24 @@
+/**
+ * Created by SteveLeeLX on 11/28/15.
+ */
+//package final_project;
+
+import javax.swing.JFrame;
+
+public class SpaceInvaders extends JFrame implements Settings {
+
+    public SpaceInvaders()
+    {
+        add(new Board());
+        setTitle("Space Invaders");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(BOARD_WIDTH, BOARD_HEIGTH);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new SpaceInvaders();
+    }
+}
