37be8d0832fa288d38d27158b83f7df3cc3b2ea6
ybz5016 <hedzyz@gmail.com>
2015-11-28 21:33:40 -0500

    bug fix

--- src/Settings.java
+++ src/Settings.java
@@ -4,7 +4,6 @@
 //package final_project;
 
 public interface Settings {
-<<<<<<< Updated upstream
     int BOARD_WIDTH = 800;
     int BOARD_HEIGHT = 600;
     int GROUND = 290;
@@ -19,20 +18,4 @@ public interface Settings {
     int DELAY = 17;
     int PLAYER_WIDTH = 15;
     int PLAYER_HEIGHT = 10;
-=======
-    public static final int BOARD_WIDTH = 358;
-    public static final int BOARD_HEIGHT = 350;
-    public static final int GROUND = 290;
-    public static final int BOMB_HEIGHT = 5;
-    public static final int ALIEN_HEIGHT = 12;
-    public static final int ALIEN_WIDTH = 12;
-    public static final int BORDER_RIGHT = 30;
-    public static final int BORDER_LEFT = 5;
-    public static final int GO_DOWN = 15;
-    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 40;
-    public static final int CHANCE = 5;
-    public static final int DELAY = 17;
-    public static final int PLAYER_WIDTH = 15;
-    public static final int PLAYER_HEIGHT = 10;
->>>>>>> Stashed changes
 }
