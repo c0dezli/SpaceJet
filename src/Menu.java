763601cd755aa71d1dad2f0ffd34f40d1b7a1b8f
ybz5016 <hedzyz@gmail.com>
2015-12-11 20:07:29 -0500

    ?How to run Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,28 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-public class Menu {
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Menu extends JPanel implements Runnable, Settings{
 
     Board board;
 
@@ -20,6 +36,62 @@ public class Menu {
     public BufferedImage quit;
     public BufferedImage bgmbutton;
 
+
+    private Dimension d;
+    private BufferedImage background = null;
+
+    private boolean ingame = true;
+    private Thread animator;
+    private Random generator = new Random();
+
+    public Menu() {
+        setFocusable(true);
+        d = new Dimension(BOARD_WIDTH, BOARD_HEIGHT);
+        BufferedImageLoader loader = new BufferedImageLoader();
+        try{
+            background = loader.loadImage("spacepix/background.png");
+        }
+        catch(IOException e){e.printStackTrace();}
+        menuInit();
+        setDoubleBuffered(true);
+    }
+
+    public void addNotify() {
+        super.addNotify();
+        menuInit();
+    }
+
+    public void menuInit() {
+        if (animator == null || !ingame) {
+            animator = new Thread(this);
+            animator.start();
+        }
+    }
+
+
+
+
+
+    public void run() {
+        long beforeTime, timeDiff, sleep;
+        beforeTime = System.currentTimeMillis();
+
+        repaint();
+
+        while (ingame) {
+            repaint();
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+            if (sleep < 0)
+                sleep = 2;
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                System.out.println("interrupted");
+            }
+            beforeTime = System.currentTimeMillis();
+        }
+    }
 
     public void render(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
@@ -36,19 +108,22 @@ public class Menu {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        if (ingame) {
+            g.drawImage(play, 100, 160, null);
+            g.drawImage(settings, 100, 230, null);
+            g.drawImage(help, 100, 300, null);
+            g.drawImage(quit, 100, 370, null);
 
-        g.drawImage(play, 100, 160, null);
-        g.drawImage(settings, 100, 230, null);
-        g.drawImage(help, 100, 300, null);
-        g.drawImage(quit, 100, 370, null);
+            g.drawImage(bgmbutton, 550, 400, null);
 
-        g.drawImage(bgmbutton, 550, 400, null);
+            Font fnt = new Font("Curlz MT", Font.BOLD, 60);
+            g.setFont(fnt);
+            g.setColor(Color.white);
+            g.drawString("Space Invaders", 70, 100);
 
-        Font fnt = new Font("Curlz MT", Font.BOLD, 60);
-        g.setFont(fnt);
-        g.setColor(Color.white);
-        g.drawString("Space Invaders", 70, 100);
-
+            Toolkit.getDefaultToolkit().sync();
+            g.dispose();
+        }
     }
 
 }
--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -3,20 +3,30 @@
  */
 //package final_project;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class SpaceInvaders extends JFrame implements Settings {
     private static int noGamesPlayed = 0;
+    private boolean ingame = true;
     public SpaceInvaders()
     {
-
-        add(new Board());
-        setTitle("Space Invaders");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(BOARD_WIDTH, BOARD_HEIGHT);
-        setLocationRelativeTo(null);
-        setVisible(true);
-        setResizable(false);
+        if (!ingame){
+            add(new Menu());
+            setTitle("Space Invaders");
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setSize(BOARD_WIDTH, BOARD_HEIGHT);
+            setLocationRelativeTo(null);
+            setVisible(true);
+            setResizable(false);
+        }   else {
+            add(new Board());
+            setTitle("Space Invaders");
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setSize(BOARD_WIDTH, BOARD_HEIGHT);
+            setLocationRelativeTo(null);
+            setVisible(true);
+            setResizable(false);
+        }
     }
 
     public static void updateGamesPlayed() {
